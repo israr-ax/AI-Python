abff9fbf126748e0df23d7c1d8600f879088e891
israr-ax <israrmemon211@gmail.com>
2026-02-19 16:01:37 +0500

    Basic Pandas problems

--- foundations/Pandas.py
+++ foundations/Pandas.py
@@ -11,3 +11,25 @@ print(df)
 print("Average Marks",df['Marks'].mean())
 print("Max marks",df["Marks"].max())
 print(df.describe())
+
+'''Create a DataFrame with: Product,Price ,Quantity
+
+Find average price Filter products with price > 500
+
+Add new column: Total = Price * Quantity '''
+
+# Create a DataFrame with: Product,Price ,Quantity
+
+data={
+    'Product': ['Soap', 'Facewash', 'Detergent', 'Shampoo'],
+    'Price' :  [210,450,550,870],
+    'Quantity': [6,5,3,12]
+}
+df=pd.DataFrame(data)
+print(df)
+# Find average price and  Filter products with price > 500
+print(f"Average Price:{df['Price'].mean()} \nFilter products with price:\n{df[df['Price']>300]}")
+
+# Add new column: Total = Price * Quantity
+df['TOTAL'] = df['Price'] * df['Quantity']
+print(df)
