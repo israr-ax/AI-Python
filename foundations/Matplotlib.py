c0dd28d3f9cd5d50df0fec9e508a12ed3a3b63ae
israr-ax <israrmemon211@gmail.com>
2026-02-21 13:52:36 +0500

    Data Visualization

--- foundations/Matplotlib.py
+++ foundations/Matplotlib.py
@@ -57,3 +57,44 @@ plt.show()
 # Check Skewed
 salary=pd.Series(salary)
 print("Skewness of data :" , salary.skew())
+
+'''
+Create a “Student Performance Dashboard”:
+
+Generate random student data:Marks,Study hours,Attendance
+Visualize:
+Histogram of marks
+Scatter (study hours vs marks)
+Bar chart (attendance categories) '''
+
+# Generate random student data:
+np.random.seed(42)
+Marks=np.random.randint(40,98,10)
+Study_Hours = np.random.randint(1,12.5 , 10)
+categories =['Present','Absent','Late','Leave']
+Attendence = np.random.randint(0,100,size=len(categories))
+    
+# Histogram of Marks
+plt.subplot(1,3,1)
+plt.hist(Marks, bins=10, edgecolor='black') 
+plt.xlabel('Marks')
+plt.ylabel('Distribution')
+plt.title('Marks distribution')
+
+# Scatter Graph (Study Hours VS Marks)
+plt.subplot(1,3,2)
+plt.scatter(Study_Hours,Marks)
+plt.xlabel('Study Hours')
+plt.ylabel('Marks')
+plt.title('Study_Hours and Marks ')
+
+#Attendence Bar Chart
+plt.subplot(1,3,3)
+plt.bar(categories, Attendence, color=['red','blue','green','orange','gray'] )
+plt.xlabel('Attendence Category')
+plt.ylabel('Count')
+plt.title('Attendence Summary')
+
+plt.suptitle("Student Dashboard")
+plt.tight_layout(rect=[0, 0, 1, 0.95])
+plt.show()
